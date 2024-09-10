656192e61b8e3e0ffff5f3b866d2e0c41eec04db
lancekasturias <lanceasturias@gmail.com>
2024-09-11 03:27:38 +0800

    update

---
+++ src/components/pixabay-api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '43844595-87d51955a21ac5c6d4436d598';
+
+export const getAPI = async (query, page) => {
+  const url = `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+
+  const response = await axios.get(url);
+
+  return response.data;
+};
